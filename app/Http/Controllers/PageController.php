1e1634bc370d2a1e3fcbb8e7ecad2c19bfccb817
Your Name <you@example.com>
2024-01-25 17:45:39 +0100

    errore

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\comic;
 
 class PageController extends Controller
 {
@@ -11,7 +12,9 @@ class PageController extends Controller
      */
     public function index()
     {
-        //
+        $comics = comic::all();
+
+        return view("comics.index");
     }
 
     /**
