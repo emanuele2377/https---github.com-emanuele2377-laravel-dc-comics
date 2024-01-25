0231049306076714d5d54ceed4a899e084421895
Your Name <you@example.com>
2024-01-25 17:03:26 +0100

    base-creata

---
+++ database/migrations/2024_01_25_153620_create_comics_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->text("description");
+            $table->boolean("is_available");
+            $table->smallInteger("page_number")->unsigned("");
+            $table->decimal("price");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comics');
+    }
+};
